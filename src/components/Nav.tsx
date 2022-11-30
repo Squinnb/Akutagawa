4bd82bb84fab54f11eff192c74ab8399ba3d2918
Quinn <squinnmb@gmail.com>
2022-11-30 13:29:11 -0600

    Nav bar dynamic language update added.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,20 +6,21 @@ import { UserContext } from "../contexts/UserContext.jsx"
 
 export default function Nav() {
   
-    const { user, toggleLang } : any = useContext(UserContext)
-    
+    const { user, toggleLang, lang } : any = useContext(UserContext)
+    const navText = {"en": ["Home", "About", "User"], "ja": ["ホーム", "アバウト", "ユーザー"]}
+
     return (
 
        <nav  id="navigation">
             <ul id="navList">
-                <li className="navli"><NavLink className="nl currentLink" to="/">ホーム </NavLink></li>
-                <li className="navli"><NavLink className="nl" to="/About">アバウト </NavLink></li>
+                <li className="navli"><NavLink className="nl currentLink" to="/">{navText[lang][0]}</NavLink></li>
+                <li className="navli"><NavLink className="nl" to="/About">{navText[lang][1]}</NavLink></li>
                 
                 {user ?
                         <>
-                        <li className="navli"><div onClick={toggleLang}>En/Ja</div></li>
+                        <li className="navli" ><button onClick={toggleLang} id='langBtn'>En/Ja</button></li>
                         <li className="navli authLink">
-                            <NavLink  id='userHomeNavLink' className="nl"  to="/UserHome"> {user.name} </NavLink>
+                            <NavLink  id='userHomeNavLink' className="nl"  to="/UserHome"> {navText[lang][2]} </NavLink>
                         </li>
                         
                         </>
