402793c62b592ecfd59e23870d1b410f12281929
Quinn <squinnmb@gmail.com>
2022-11-30 18:15:44 -0600

    updated vite config

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -11,12 +11,13 @@ interface Props {
 const SearchForm: React.FC<Props> = (props) => {
 
     const { books, sw } = props
-    const [query, setQuery] = useState("")
+    let [query, setQuery] = useState("")
     const { lang } : any = useContext(UserContext)
   
     const handleChange = (e: any) => {
         let sq = e.target.value
         setQuery(sq)
+        console.log("q: ", query, sq)
     }
 
     const handleSearch = (e: any) => {
@@ -30,7 +31,7 @@ const SearchForm: React.FC<Props> = (props) => {
     }
 
     const pholderText = {"en": "Ex: Author, Title", "ja": "例：受賞者、受賞作"}
-const btnText = {"en": "Search", "ja": "検索"}
+    const btnText = {"en": "Search", "ja": "検索"}
 
     return (
         <form onSubmit={handleSearch}>
