2a230198583bab72bda03e26e3d48274b91dd3b4
Quinn Barnett <squinnmb@gmail.com>
2023-05-28 16:35:56 -0500

    Make review form text multilingual

--- src/components/BookDetailComponents/ReviewForm.tsx
+++ src/components/BookDetailComponents/ReviewForm.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {useState, useContext} from 'react'
 
 import '../../details.css'
+import { UserContext } from '../../contexts/UserContext';
 
 interface Props {
     reviewTitle: string;
@@ -13,6 +14,8 @@ interface Props {
 
 export default function ReviewForm(props: Props) {
     const { setReviewText, setReviewTitle, reviewText, reviewTitle } = props
+    const { lang } : any = useContext(UserContext)
+    const reviewFormText = {ja: {revTitle: "レビュータイトル", revBody: "レビュー文章", revSubmit: "レビュー送信"}, en: {revTitle: "Review Title", revBody: "Review Body", revSubmit: "Submit Review"}}
 
     const handleChange = (e: any) => {
         if(e.target.name === "reviewTitle") { setReviewTitle(e.target.value) }
@@ -20,15 +23,16 @@ export default function ReviewForm(props: Props) {
     }
 
     const handleSubmit = (e: any) => {
-        e.preventDefault()
-        
+        e.preventDefault() 
     }
+
+    
 
     return (
         <form action="submit" onSubmit={(e) => handleSubmit(e)} className='review_form'>
-            <input className='review_form_title' value={reviewTitle} type="text" name='reviewTitle' onChange={(e) => handleChange(e)} placeholder='レビュータイトル' />
-            <textarea className='review_form_text' value={reviewText} name="reviewText"  onChange={(e) => handleChange(e)}　placeholder='レビュー文章'> </textarea>
-            <button className='review_form_btn'>レビュー送信</button>
+            <input className='review_form_title' value={reviewTitle} type="text" name='reviewTitle' onChange={(e) => handleChange(e)} placeholder={reviewFormText[lang].revTitle} />
+            <textarea className='review_form_text' value={reviewText} name="reviewText"  onChange={(e) => handleChange(e)}　placeholder={reviewFormText[lang].revBody}> </textarea>
+            <button className='review_form_btn'>{reviewFormText[lang].revSubmit}</button>
         </form>
     )
 }
